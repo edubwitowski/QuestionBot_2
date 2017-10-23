4315e816581882bc5a62d6790f681c695d2eec03
Eric Witowski <edubwitowski@gmail.com>
2017-10-23 13:06:13 -0500

    Initial Commit

---
+++ QuestionBot_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuestionBot_2
+//
+//  Created by User on 10/23/17.
+//  Copyright © 2017 ewitowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
